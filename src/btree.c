72c929a01bf2d21608b7722d6a45112894d1c3e0
SejDreng <114228762+SejDreng@users.noreply.github.com>
2022-11-11 10:28:22 +0100

    Afleveret

--- src/btree.c
+++ src/btree.c
@@ -7,6 +7,9 @@
 
 struct tree_node *Insert(int x, struct tree_node *t) {
   // Insert item x into the tree t
+
+  // Making a recursion through the tree until the right place for the inserted item is found.
+  // Once the right place is found a new tree_node is mad for the item.
   if(t == NULL)
   {
     printf("%d inserted \n",x);
@@ -33,27 +36,35 @@ struct tree_node *Insert(int x, struct tree_node *t) {
 
 struct tree_node *Remove(int x, struct tree_node *t) {
   // Remove one item from the tree t
+  // Again, using recursion to navigate through the tree to find the wished item to remove.
+  // Once found, the position of the item in the tree needs to be asserted, so that it can be removed in the right way.
   if(t == NULL)
   {
     return NULL;
   }
   if(x == t->item)
   {
+    // If the leaf has no children, then it just needs to be removed.
     if(t->left == NULL && t->right == NULL)
     {
       t = NULL;
       free(t);
     }
+    // If the leaf has left child and not a right child, then the left branch needs to be mooved down a notch to that the left child is the new t.
     else if(t->left != NULL && t->right == NULL)
     {
       t = t->left;
       free(t->left);
     }
+    // If it's the opposite case compared to the one above, than it's the same, just with the right side.
     else if(t->left == NULL && t->right != NULL)
     {
       t = t->right;
       free(t->right);
     }
+    // If theres both a right and a left branch, than we needto do some more. 
+    // We can solve it by going one step down the left branch, and then by going as far right ads possible, we can use that leaf as the new t.
+    // It can also be done mirrored. 
     else
     {
       if(t->left->right != NULL)
@@ -96,6 +107,7 @@ struct tree_node *Remove(int x, struct tree_node *t) {
 int Contains(int x, struct tree_node *t) {
 
   // Return true if the tree t contains item x. Return false otherwise.
+  // Recursing through the tree until the right element is found. Otherwise, it's not there. 
   if(t == NULL)
   {
     return 0;
